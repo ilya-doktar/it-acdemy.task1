d20bbeca65da57964797d8d1fd54f6e313397c24
Ilya Gorbachev <ilya_gorbachev@mail.ru>
2020-12-09 20:00:41 +0300

    homeWork2

--- HomeWork/src/HomeWork2/Loops2.java
+++ HomeWork/src/HomeWork2/Loops2.java
@@ -4,26 +4,32 @@ import java.util.Objects;
 
 public class Loops2 {
     public static void main(String[] args) {
-        getMultiply(4123212);
+        System.out.println(getMultiply(412321253));
     }
-    public static void getMultiply(int a){
+    public static String getMultiply(int a){
+        if( a % a !=0){
+            System.out.println("Введено не целое число");
+            return "";
+        }
+
         String out = "";
         int result = 1;
         int j = 1;
         int b = a;
+
        while ( b/10 > 0) {
            b = b/10;
            j++;
        }
-       System.out.println("Count of numbers: " + j);
+
        for (int i = 0; i < j; i++) {
            int c = a % 10;
            a = a / 10;
            result = result * c;
-           out = out + c + " * ";
+           out = out +  " * " + c;
        }
         out = out + "= " + result;
-        System.out.println(out);
+       return out;
     }
 }
 
