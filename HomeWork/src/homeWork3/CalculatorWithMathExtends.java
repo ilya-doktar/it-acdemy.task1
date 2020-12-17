cf2ed7c48235086fab8ffa9d24ea90677f51d5e0
Ilya Gorbachev <ilya_gorbachev@mail.ru>
2020-12-17 13:54:39 +0300

    Override

--- HomeWork/src/homeWork3/CalculatorWithMathExtends.java
+++ HomeWork/src/homeWork3/CalculatorWithMathExtends.java
@@ -1,4 +1,18 @@
 package homeWork3;
 
 public class CalculatorWithMathExtends extends CalculatorWithOperator {
+    public double getPow(double a, int b){
+        double result = Math.pow(a,b);
+        return result;
+    }
+
+    public int getAbs(int a){
+        int result = Math.abs(a);
+        return result;
+    }
+
+    public double getSqrt(double a){
+        double result = Math.sqrt(a);
+        return result;
+    }
 }
